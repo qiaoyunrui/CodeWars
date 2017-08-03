6741a64bd9fb487bcf390cf5c4ca64c11451c064
Juhezi <juhezix@163.com>
2017-08-04 00:51:46 +0800

    [Update] BaseActivity views

--- base/src/main/java/me/juhezi/module/base/BaseActivity.kt
+++ base/src/main/java/me/juhezi/module/base/BaseActivity.kt
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
 import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.FrameLayout
 import butterknife.Unbinder
 
@@ -16,17 +17,14 @@ import butterknife.Unbinder
 open class BaseActivity : AppCompatActivity() {
 
     companion object {
-        const val STATUS_CONTENT: Long = 0x00
-        const val STATUS_LOADING: Long = 0x01
-        const val STATUS_EMPTY: Long = 0x02
-        const val STATUS_ERROR: Long = 0x03
-        const val STATUS_NO_NETWORK: Long = 0x04
+        const val STATUS_CONTENT = 0x00
+        const val STATUS_LOADING = 0x01
+        const val STATUS_EMPTY = 0x02
+        const val STATUS_ERROR = 0x03
+        const val STATUS_NO_NETWORK = 0x04
     }
 
-    @IntDef(STATUS_LOADING, STATUS_CONTENT, STATUS_EMPTY, STATUS_ERROR, STATUS_NO_NETWORK)
-    annotation class ViewStatus
-
-    @ViewStatus var mStatus = STATUS_EMPTY
+    var mStatus = STATUS_EMPTY
 
     //多种状态的 View
     private var mEmptyView: View? = null
@@ -34,6 +32,9 @@ open class BaseActivity : AppCompatActivity() {
     private var mLoadingView: View? = null
     private var mNoNetworkView: View? = null
     private var mContentView: View? = null
+
+    private val mLayoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.MATCH_PARENT)
 
     @LayoutRes private var mEmptyViewResId = android.R.layout.activity_list_item
     @LayoutRes private var mErrorViewResId = android.R.layout.activity_list_item
@@ -56,14 +57,74 @@ open class BaseActivity : AppCompatActivity() {
         mInflater = LayoutInflater.from(this)
         initBaseData()
         installViews()
-
     }
 
-    private fun initBaseData() {}
+    private fun initBaseData() {
+        super.setContentView(mRootViewResId)
+        mRootView = findViewById(R.id.vg_base_activity_root)
+        mContainer = findViewById(R.id.vg_base_activity_container) as FrameLayout?
+        mToolbar = findViewById(R.id.tb_base_activity) as Toolbar?
+    }
+
+    override fun setContentView(layoutResID: Int) {
+        mContentView = mInflater?.inflate(layoutResID, null)
+        mContainer?.addView(mContentView, 0, mLayoutParams)
+    }
 
     protected fun installViews() {}
 
     protected fun onRefresh() {}
+
+    protected fun showContentView() {
+        mStatus = STATUS_CONTENT
+        showView(mStatus)
+    }
+
+    protected fun showLoading(layoutResID: Int = mLoadingViewResId, action: ((View?) -> Unit)? = null) {
+        mStatus = STATUS_LOADING
+        //Load View
+        action?.invoke(mLoadingView)
+        showView(mStatus)
+    }
+
+    protected fun showError(layoutResID: Int = mErrorViewResId, action: ((View?) -> Unit)? = null) {
+        mStatus = STATUS_ERROR
+        action?.invoke(mErrorView)
+        showView(mStatus)
+    }
+
+    protected fun showNoNetWork(layoutResID: Int = mNoNetworkViewResId, action: ((View?) -> Unit)? = null) {
+        mStatus = STATUS_NO_NETWORK
+        action?.invoke(mNoNetworkView)
+        showView(mStatus)
+    }
+
+    protected fun showEmpty(layoutResID: Int = mEmptyViewResId, action: ((View?) -> Unit)? = null) {
+        mStatus = STATUS_EMPTY
+        action?.invoke(mEmptyView)
+        showView(mStatus)
+    }
+
+    private fun showView(viewStatus: Int) {
+        when (viewStatus) {
+            STATUS_CONTENT -> mContentView?.visibility = View.VISIBLE
+            STATUS_EMPTY -> mEmptyView?.visibility = View.VISIBLE
+            STATUS_ERROR -> mErrorView?.visibility = View.VISIBLE
+            STATUS_NO_NETWORK -> mErrorView?.visibility = View.VISIBLE
+            STATUS_LOADING -> mLoadingView?.visibility = View.VISIBLE
+            else -> mEmptyView?.visibility = View.VISIBLE
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mContentView = null
+        mEmptyView = null
+        mErrorView = null
+        mLoadingView = null
+        mNoNetworkView = null
+        mUnBinder?.unbind()
+    }
 
 }
 
