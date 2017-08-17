f176a506eca344661d53e6f36ee8a5a22582ee4c
Juhezi <juhezix@163.com>
2017-08-17 12:21:59 +0800

    [Update] update gradle for Android Studio 3.0

--- app/src/main/java/me/juhezi/codewars/MainActivity.kt
+++ app/src/main/java/me/juhezi/codewars/MainActivity.kt
@@ -1,12 +1,7 @@
 package me.juhezi.codewars
 
-import android.support.v7.app.AppCompatActivity
-import android.os.Bundle
+import me.juhezi.module.base.BaseActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
 }
