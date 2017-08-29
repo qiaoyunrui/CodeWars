e427861728d88ea976ca8b14207bf966767b7058
Juhezi <juhezix@163.com>
2017-08-29 15:34:01 +0800

    [Update] Activity turn with data

--- base/src/main/java/me/juhezi/module/base/router/Functions.kt
+++ base/src/main/java/me/juhezi/module/base/router/Functions.kt
@@ -10,9 +10,9 @@ import me.juhezi.module.base.router.activity.turn
  */
 fun Intent.modify(action: Intent.() -> Unit) = apply(action)
 
-fun buildIntent(context: Context, key: String): Intent {
+fun buildIntent(context: Context, key: String, action: Intent.() -> Unit = {}): Intent {
     val clazz = Repository.getActivityClass(key)    // -> 这里需要修改
-    return IntentBuilder(context, clazz).build()
+    return IntentBuilder(context, clazz).build().modify(action)
 }
 
 fun Any.register() = Repository.register(this::class.java)
--- base/src/main/java/me/juhezi/module/base/router/activity/Ex.kt
+++ base/src/main/java/me/juhezi/module/base/router/activity/Ex.kt
@@ -1,6 +1,7 @@
 package me.juhezi.module.base.router.activity
 
 import android.app.Activity
+import android.content.Intent
 import android.support.v4.app.Fragment
 import me.juhezi.module.base.router.buildIntent
 
@@ -8,15 +9,12 @@ import me.juhezi.module.base.router.buildIntent
  * Created by Juhezi on 2017/8/17.
  */
 
-//startActivity
-fun Activity.turn(key: String) = startActivity(buildIntent(this, key)) //这个是最基本的跳转
+fun Activity.turn(key: String, action: Intent.() -> Unit = {}) = startActivity(buildIntent(this, key, action)) //这个是最基本的跳转
 
-fun Activity.turnForResult(key: String) {
+fun Activity.turnForResult(key: String, requestCode: Int, action: Intent.() -> Unit = {}) =
+        startActivityForResult(buildIntent(this, key, action), requestCode)
 
-}
+fun Fragment.turn(key: String, action: Intent.() -> Unit = {}) = startActivity(buildIntent(context, key, action))
 
-fun Fragment.turn(key: String) = startActivity(buildIntent(context, key))
-
-fun Fragment.turnForResult(key: String) {
-
-}
+fun Fragment.turnForResult(key: String, requestCode: Int, action: Intent.() -> Unit = {}) =
+        startActivityForResult(buildIntent(context, key, action), requestCode)
