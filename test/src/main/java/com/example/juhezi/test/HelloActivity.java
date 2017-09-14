f7f4ad5bd46671caf8a47f93cd29b08ab188e5a5
Juhezi <juhezix@163.com>
2017-09-14 16:11:29 +0800

    [Test] Blur Success

--- test/src/main/java/com/example/juhezi/test/HelloActivity.java
+++ test/src/main/java/com/example/juhezi/test/HelloActivity.java
@@ -13,6 +13,8 @@ import butterknife.Unbinder;
 import me.juhezi.module.base.BaseActivity;
 import me.juhezi.module.base.utils.BlurUtils;
 
+import static me.juhezi.module.base.functions.FunctionAKt.runInUIThread;
+
 /**
  * Created by Juhezi[juhezix@163.com] on 2017/9/3.
  */
@@ -29,10 +31,8 @@ public class HelloActivity extends BaseActivity {
         setContentView(R.layout.layout_hello);
         showContentView();
         mUnbinder = ButterKnife.bind(this);
-        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.picture);
-        bitmap = BlurUtils.blur(bitmap, 50);
-//        Log.i("TAG", "installViews: " + mIvBackground);
-        mIvBackground.setImageBitmap(bitmap);
+        SubThread thread = new SubThread(mIvBackground);
+        thread.start();
     }
 
     @Override
@@ -45,15 +45,22 @@ public class HelloActivity extends BaseActivity {
 
     class SubThread extends Thread {
 
-        private Bitmap mBitmap;
+        private ImageView mImageView;
 
-        public SubThread(Bitmap bitmap) {
-            this.mBitmap = bitmap;
+        public SubThread(ImageView imageView) {
+            this.mImageView = imageView;
         }
 
         @Override
         public void run() {
-            super.run();
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.picture);
+            final Bitmap newBitmap = BlurUtils.blur(bitmap, 50);
+            runInUIThread(new Runnable() {
+                @Override
+                public void run() {
+                    mImageView.setImageBitmap(newBitmap);
+                }
+            });
         }
     }
 
