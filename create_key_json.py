1dcbb90ba7bee8bfb0b74a378fc9117b45f86501
Juhezi <juhezix@163.com>
2017-11-04 10:58:36 +0800

    [Add] script to create keys json file

---
+++ create_key_json.py
@@ -0,0 +1,33 @@
+import os
+import json
+
+fileName = ".keys.json"
+
+if os.path.exists(fileName):
+    print("File has existed!")
+    exit()
+
+keys = {}
+
+KEYSTORE_PASS = 'KEYSTORE_PASS'
+ALIAS_NAME = 'ALIAS_NAME'
+ALIAS_PASS = 'ALIAS_PASS'
+
+DESC_STRING = "Please input '%s':"
+
+print(DESC_STRING % KEYSTORE_PASS)
+keystorePassword = input()
+print(DESC_STRING % ALIAS_NAME)
+aliasName = input()
+print(DESC_STRING % ALIAS_NAME)
+aliasPassword = input()
+keys[KEYSTORE_PASS] = keystorePassword
+keys[ALIAS_NAME] = aliasName
+keys[ALIAS_PASS] = aliasPassword
+
+jsonContent = json.dumps(keys)
+
+with open(fileName, "w") as f:
+    f.write(jsonContent)
+
+print("File '.keys.json' has been created.")
