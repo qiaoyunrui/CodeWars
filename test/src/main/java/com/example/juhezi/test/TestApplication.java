bf02c481c9aabac44ad28e80dd68937e5596f482
Juhezi <juhezix@163.com>
2017-11-30 14:48:14 +0800

    [Update]

--- test/src/main/java/com/example/juhezi/test/TestApplication.java
+++ test/src/main/java/com/example/juhezi/test/TestApplication.java
@@ -2,7 +2,6 @@ package com.example.juhezi.test;
 
 import com.juhezi.module.router_annotation.annotation.Proxy;
 
-import me.juhezi.module.base.BaseActivity;
 import me.juhezi.module.base.BaseApplication;
 import me.juhezi.module.router_api.Router;
 
