9a71d8dc2891dd71a84dafe0422afe7dc33cca1b
qiaoyunrui <shixi_yunrui@staff.weibo.com>
2018-01-10 23:37:39 +0800

    [Add] lottie

--- ding/src/main/java/me/juhezi/ding/ui/TurnActivity.kt
+++ ding/src/main/java/me/juhezi/ding/ui/TurnActivity.kt
@@ -1,12 +1,19 @@
 package me.juhezi.ding.ui
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.support.v7.app.AppCompatActivity
+import com.airbnb.lottie.LottieComposition
 import kotlinx.android.synthetic.main.activity_turn.*
 import me.juhezi.ding.R
+import me.juhezi.module.base.extensions.i
+import java.io.File
 
 /**
+ *
  * Created by Juhezi on 2018/1/3.
  */
 class TurnActivity : AppCompatActivity() {
@@ -19,10 +26,23 @@ class TurnActivity : AppCompatActivity() {
 
     fun initEvent() {
         btn_capture.setOnClickListener {
-
+            //调用系统相机拍摄
+            val intent = Intent()
+            intent.action = MediaStore.ACTION_IMAGE_CAPTURE
+            intent.addCategory(Intent.CATEGORY_DEFAULT)
+            val file = File("${Environment.getExternalStorageDirectory()}/123.mp4")
+            i(file.path)
+            val uri = Uri.fromFile(file)
+            intent.putExtra(MediaStore.EXTRA_OUTPUT,uri)
+            startActivity(intent)
         }
         btn_pick_from_photo_album.setOnClickListener {
-            //打开相机录像
+            //从相册选择
+        }
+//        lav_anim.setAnimation("anim.json")
+        lav_anim.imageAssetsFolder = "images"
+        fab_add.setOnClickListener{
+            lav_anim.playAnimation()
         }
     }
 
